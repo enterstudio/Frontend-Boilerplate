2ff2f909a115faa7ab54fd191b45bcd4cfb73918
Ross Bennett <ross.bennett@ldnarc-dmxfhbj.emea.corp.ipgnetwork.com>
2015-03-10 17:34:51 +0000

    chore(*): add head placeholders

--- public/index.php
+++ public/index.php
@@ -9,10 +9,15 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Favicons -->
+
         <!-- Humans.txt -->
         <link type="text/plain" rel="author" href="/humans.txt" />
 
         <!-- Styles -->
+        <link rel="stylesheet" href="_css/screen.css">
+
     </head>
     <body>
         <!-- inject:svg -->
