8d1b2cb6848b38f7f77298fc92ccd0c72ae80ceb
Adam Onishi <aonishi@wearearchitect.com>
2015-03-23 09:49:12 +0000

    fix(gulp): Fix issues with imgmin task and add cache clear task

--- gulpfile.js
+++ gulpfile.js
@@ -3,67 +3,67 @@
  */
 
 /*-----------------------------------------*\
-    VARIABLES
+  VARIABLES
 \*-----------------------------------------*/
 
 // Dependencies
 var $ = require('gulp-load-plugins')(),
-	gulp = require('gulp'),
-	del = require('del'),
-	fs = require('fs'),
-	lazypipe = require('lazypipe'),
+  gulp = require('gulp'),
+  del = require('del'),
+  fs = require('fs'),
+  lazypipe = require('lazypipe'),
   argv = require('yargs').argv,
-	pngquant = require('imagemin-pngquant'),
-	runSequence = require('run-sequence'),
-	browserSync = require('browser-sync'),
-	reload = browserSync.reload,
+  pngquant = require('imagemin-pngquant'),
+  runSequence = require('run-sequence'),
+  browserSync = require('browser-sync'),
+  reload = browserSync.reload,
 
   // Environments
   production = !!(argv.production), // true if --prod flag is used
 
-	// Base Paths
-	basePaths = {
-		src: 'assets/',
-		dest: 'public/'
-	},
+  // Base Paths
+  basePaths = {
+    src: 'assets/',
+    dest: 'public/'
+  },
 
-	// Assets Folder Paths
-	paths = {
-		scss: basePaths.src + 'scss/**/*.scss',
-		js: {
-			src: basePaths.src + 'js/src/**/*.js',
-			vendor: basePaths.src + 'js/vendor/*.js'
-		},
-		img: basePaths.src + 'img/**/*'
-	},
+  // Assets Folder Paths
+  paths = {
+    scss: basePaths.src + 'scss/**/*.scss',
+    js: {
+      src: basePaths.src + 'js/src/**/*.js',
+      vendor: basePaths.src + 'js/vendor/*.js'
+    },
+    img: basePaths.src + 'img/**/*'
+  },
 
 
 /*-----------------------------------------*\
-    ERROR NOTIFICATION
-    - Beep!
+  ERROR NOTIFICATION
+  - Beep!
 \*-----------------------------------------*/
 
 onError = function(err) {
-	$.notify.onError({
-		title: "Gulp",
-		subtitle: "Failure!",
-		message: "Error: <%= error.message %>",
-		sound: "Beep"
-	})(err);
-	this.emit('end');
+  $.notify.onError({
+    title: "Gulp",
+    subtitle: "Failure!",
+    message: "Error: <%= error.message %>",
+    sound: "Beep"
+  })(err);
+  this.emit('end');
 };
 
 
 /*-----------------------------------------*\
-    BROWSER SYNC
-    - View project at test.dev:3000
+  BROWSER SYNC
+  - View project at test.dev:3000
 \*-----------------------------------------*/
 
 gulp.task('browser-sync', function() {
-	browserSync({
-		proxy: "test.dev",
-		notify: false
-	});
+  browserSync({
+    proxy: "test.dev",
+    notify: false
+  });
 });
 
 
@@ -74,35 +74,35 @@ gulp.task('browser-sync', function() {
    - Vendor prefix
    - Output unminified CSS for debugging
    - Rename
-	 - Minify
-	 - Output minified CSS
+   - Minify
+   - Output minified CSS
 \*-----------------------------------------*/
 
 gulp.task('styles', function () {
   return gulp.src(paths.scss)
-		.pipe( $.plumber({errorHandler: onError}) )
+    .pipe( $.plumber({errorHandler: onError}) )
     .pipe($.sass({ style: 'expanded', }))
-		.pipe( $.autoprefixer('last 2 version') )
-		.pipe( gulp.dest(basePaths.dest + '_css') )
-		.pipe( $.rename({ suffix: '.min' }) )
-		.pipe( $.minifyCss() )
-		.pipe( gulp.dest(basePaths.dest + '_css') )
-		.pipe( $.size({title: 'Styles'}));
+    .pipe( $.autoprefixer('last 2 version') )
+    .pipe( gulp.dest(basePaths.dest) )
+    .pipe( $.rename({ suffix: '.min' }) )
+    .pipe( $.minifyCss() )
+    .pipe( gulp.dest(basePaths.dest) )
+    .pipe( $.size({title: 'Styles'}));
 });
 
 /*-----------------------------------------*\
-    SASS LINTING
-    - Keep your code squeaky clean
+  SASS LINTING
+  - Keep your code squeaky clean
 \*-----------------------------------------*/
 
 gulp.task('lint', function() {
-	return gulp.src(paths.scss)
-	.pipe( $.plumber({errorHandler: onError}) )
-	.pipe( $.scssLint( {
-		'bundleExec': true,
-		'config': '.scss-lint.yml',
-		'reporterOutput': 'scss-lint-report.xml'
-	}));
+  return gulp.src(paths.scss)
+  .pipe( $.plumber({errorHandler: onError}) )
+  .pipe( $.scssLint( {
+    'bundleExec': true,
+    'config': '.scss-lint.yml',
+    'reporterOutput': 'scss-lint-report.xml'
+  }));
 });
 
 
@@ -113,21 +113,21 @@ gulp.task('lint', function() {
    - Concatenate assets/js into core.js
    - Output unminified JS for debugging
    - Minify
-	 - Rename
-	 - Output minified JS
+   - Rename
+   - Output minified JS
 \*-----------------------------------------*/
 
 gulp.task('scripts',function(){
-	gulp.src(paths.js.src)
-	.pipe( $.if(!production, $.plumber({errorHandler: onError}) ))
-	.pipe( $.if(!production, $.jshint() ))
-	.pipe( $.if(!production, $.jshint.reporter('default') ))
-	.pipe( $.concat('core.js') )
-	.pipe( gulp.dest(basePaths.dest + '_js') )
-	.pipe( $.uglify() )
-	.pipe( $.rename({ suffix: '.min' }) )
-	.pipe( gulp.dest(basePaths.dest + '_js') )
-	.pipe( $.size({title: 'Scripts'}));
+  gulp.src(paths.js.src)
+  .pipe( $.if(!production, $.plumber({errorHandler: onError}) ))
+  .pipe( $.if(!production, $.jshint() ))
+  .pipe( $.if(!production, $.jshint.reporter('default') ))
+  .pipe( $.concat('core.js') )
+  .pipe( gulp.dest(basePaths.dest + 'javascript') )
+  .pipe( $.uglify() )
+  .pipe( $.rename({ suffix: '.min' }) )
+  .pipe( gulp.dest(basePaths.dest + 'javascript') )
+  .pipe( $.size({title: 'Scripts'}));
 });
 
 
@@ -139,10 +139,10 @@ gulp.task('scripts',function(){
 \*-----------------------------------------*/
 
 gulp.task('vendorScripts',function(){
-	return gulp.src(paths.js.vendor)
-	.pipe($.uglify())
-	.pipe(gulp.dest(basePaths.dest + '_js/vendor'))
-	.pipe($.size({title: 'Vendor Scripts'}));
+  return gulp.src(paths.js.vendor)
+  .pipe($.uglify())
+  .pipe(gulp.dest(basePaths.dest + 'javascript'))
+  .pipe($.size({title: 'Vendor Scripts'}));
 });
 
 
@@ -153,14 +153,12 @@ gulp.task('vendorScripts',function(){
 \*-----------------------------------------*/
 
 gulp.task('imgmin', function () {
-	return gulp.src(paths.img)
-	  .pipe(stripAttrs())
-		.pipe( $.if(!production, $.cache( $.imagemin({
-				progressive: true,
-				svgoPlugins: [{removeViewBox: false}],
-				use: [ pngquant() ]
-			}))))
-		.pipe( gulp.dest(basePaths.dest + '_img'));
+  return gulp.src(paths.img)
+    .pipe( $.cache( $.imagemin({
+        progressive: true,
+        use: [ pngquant() ]
+      })))
+    .pipe( gulp.dest(basePaths.dest + 'images'));
 });
 
 
@@ -181,12 +179,12 @@ gulp.task('imgmin', function () {
 
 var svgPaths = {
   images: {
-    src: basePaths.src + 'img/svg',
-    dest: '_img'
+    src: basePaths.src + 'svg',
+    dest: 'images'
   },
   sprite: {
     src: basePaths.src + 'img/svg/*.svg',
-    svgSymbols: '_img/svg/icons/icons.svg',
+    svgSymbols: 'svg/icons.svg'
   }
 };
 
@@ -194,29 +192,27 @@ var svgPaths = {
 // Create SVG Symbols for icons.
 gulp.task('svgSymbols', function () {
   return gulp.src(svgPaths.sprite.src)
-    .pipe(stripAttrs())
-    .pipe($.svgSprite(
-      {
-        mode        : {
-	        symbol      : {
-	        prefix      : ".icon-%s",
-	        dimensions  : "%s",
-	        sprite      : "svg/icons/icons.svg",
-	        dest        : svgPaths.images.dest,
-	        inline 			: true,
-	        "example": {
-	          "dest": "svg/icons/icons-preview.html"
-	          }
-	        }
-      },
-        svg                     : {
-          dimensionAttributes : false
+  .pipe( stripAttrs() )
+  .pipe( $.svgSprite({
+    mode : {
+        symbol     : {
+        prefix     : ".icon-%s",
+        dimensions : "%s",
+        sprite     : "svg/icons/icons.svg",
+        dest       : svgPaths.images.dest,
+        inline     : true,
+        "example"  : {
+          "dest": "icons-preview.html"
         }
       }
-    ))
-    .pipe(gulp.dest(basePaths.dest))
-    .pipe($.size({title: 'SVG Symbols'}));
-  });
+    },
+    svg : {
+      dimensionAttributes : false
+    }
+  }))
+  .pipe(gulp.dest(basePaths.dest))
+  .pipe($.size({title: 'SVG Symbols'}));
+});
 
 // SVG Document Injection
 // Inject SVG <symbol> block just after opening <body> tag.
@@ -245,7 +241,7 @@ gulp.task('svg', function(cb) {
 \*-----------------------------------------*/
 
 gulp.task('dev', function() {
-    gulp.start('scripts', 'styles');
+  gulp.start('scripts', 'styles');
 });
 
 
@@ -254,12 +250,16 @@ gulp.task('dev', function() {
 \*-----------------------------------------*/
 
 gulp.task('clean', function() {
-  if( !production )
-  	del([basePaths.dest + '_*'], { read: false });
-  else
-    del([basePaths.dest + '_*', '!' + basePaths.dest + '_img' ], { read: false })
+  del([basePaths.dest + 'javascript', basePaths.dest + 'images'], { read: false });
 });
 
+/*-----------------------------------------*\
+   CLEAR CACHE
+\*-----------------------------------------*/
+
+gulp.task('clear', function (done) {
+  return $.cache.clearAll(done);
+});
 
 /*-----------------------------------------*\
    MANUAL DEFAULT TASK
@@ -268,7 +268,7 @@ gulp.task('clean', function() {
 \*-----------------------------------------*/
 
 gulp.task('default', ['clean'], function(cb) {
-	runSequence('styles', ['scripts', 'vendorScripts', 'imgmin'], 'svg', cb);
+  runSequence('styles', ['scripts', 'vendorScripts', 'imgmin'], 'svg', cb);
 });
 
 /*-----------------------------------------*\
@@ -278,9 +278,9 @@ gulp.task('default', ['clean'], function(cb) {
 \*-----------------------------------------*/
 
 gulp.task('watch', ['browser-sync'], function() {
-	gulp.watch(paths.scss, ['styles', reload]);
-	gulp.watch(paths.js.src, ['scripts', reload]);
-	gulp.watch([basePaths.dest + '*.html', basePaths.dest + '*.php'], reload);
+  gulp.watch(paths.scss, ['styles', reload]);
+  gulp.watch(paths.js.src, ['scripts', reload]);
+  gulp.watch([basePaths.dest + '*.html', basePaths.dest + '*.php'], reload);
 });
 
 /*-----------------------------------------*\
@@ -290,12 +290,9 @@ gulp.task('watch', ['browser-sync'], function() {
 
 // Strips attributes from SVGs
 var stripAttrs = lazypipe()
-  .pipe(
-    $.cheerio,
-    {
-      run: function ($) {
-        $('[fill]').removeAttr('fill');
-      },
-      parserOptions: { xmlMode: true }
-    }
-  );
+  .pipe( $.cheerio, {
+    run: function ($) {
+      $('[fill]').removeAttr('fill');
+    },
+    parserOptions: { xmlMode: true }
+  });
